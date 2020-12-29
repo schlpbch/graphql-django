b8e84581edb746b5f5e7de212fc0556abc3002ea
Andreas Schlapbach <schlpbch@gmail.com>
2020-12-29 21:03:55 +0100

    Update to 2.2.17

--- hackernews/links/models.py
+++ hackernews/links/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.conf import settings
 
+
 class Link(models.Model):
     url = models.URLField()
     description = models.TextField(blank=True)
-    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    posted_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return self.url
+
 
 class Vote(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    link = models.ForeignKey('links.Link', related_name='votes', on_delete=models.CASCADE)
+    link = models.ForeignKey(
+        "links.Link", related_name="votes", on_delete=models.CASCADE
+    )
